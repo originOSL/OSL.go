ea897d5eeacd09669821524caaacc644adc09722
Mistium <d.a.kimber@icloud.com>
2026-04-15 17:59:35 +0100

    fix cmd.Run

--- packages/cmd.go
+++ packages/cmd.go
@@ -1,12 +1,11 @@
 // name: cmd
 // description: Command line utilities
 // author: Mist
-// requires: os, strings
+// requires: os, strings, os/exec
 
 type Cmd struct{}
 
 func (Cmd) Run(cmd string, args ...string) string {
-	cmdArgs := append([]string{cmd}, args...)
 	output, err := exec.Command(cmd, args...).CombinedOutput()
 	if err != nil {
 		return ""
