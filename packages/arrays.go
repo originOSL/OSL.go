db381fde3970b662294867142b1e485f803c6de7
Mistium <d.a.kimber@icloud.com>
2026-04-15 16:11:33 +0100

    new arrays methods

--- packages/arrays.go
+++ packages/arrays.go
@@ -34,4 +34,76 @@ func (Arrays) SortByNum(arr []any, key string) []any {
 	return arr
 }
 
+func (Arrays) Filter(arr []any, fn func(any) bool) []any {
+	var filtered []any
+	for _, v := range arr {
+		if fn(v) {
+			filtered = append(filtered, v)
+		}
+	}
+	if len(filtered) == 0 {
+		return []any{}
+	}
+	return filtered
+}
+
+func (Arrays) Map(arr []any, fn func(any) any) []any {
+	var mapped []any
+	for _, v := range arr {
+		mapped = append(mapped, fn(v))
+	}
+	if len(mapped) == 0 {
+		return []any{}
+	}
+	return mapped
+}
+
+func (Arrays) Reduce(arr []any, fn func(any, any) any, initial any) any {
+	for _, v := range arr {
+		initial = fn(initial, v)
+	}
+	return initial
+}
+
+func (Arrays) ReduceRight(arr []any, fn func(any, any) any, initial any) any {
+	for i := len(arr) - 1; i >= 0; i-- {
+		initial = fn(initial, arr[i])
+	}
+	return initial
+}
+
+func (Arrays) Reverse(arr []any) []any {
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+	return arr
+}
+
+func (Arrays) Find(arr []any, fn func(any) bool) any {
+	for _, v := range arr {
+		if fn(v) {
+			return v
+		}
+	}
+	return nil
+}
+
+func (Arrays) FindIndex(arr []any, fn func(any) bool) int {
+	for i, v := range arr {
+		if fn(v) {
+			return i
+		}
+	}
+	return -1
+}
+
+func (Arrays) FindLastIndex(arr []any, fn func(any) bool) int {
+	for i, v := range arr {
+		if fn(v) {
+			return i
+		}
+	}
+	return -1
+}
+
 var arrays = Arrays{}
